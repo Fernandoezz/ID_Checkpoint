8118fc9952f7aa292298a15c4b11379771c7c7b1
PrameshFernando <fernandomps.21@uom.lk>
2025-03-05 10:19:42 +0530

    Addedvfrontend and backend projects

---
+++ backend/id-checkpoint/src/main/java/com/Fernandoezz/userservice/id_checkpoint/IdCheckpointApplication.java
@@ -0,0 +1,13 @@
+package com.Fernandoezz.userservice.id_checkpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdCheckpointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdCheckpointApplication.class, args);
+	}
+
+}
---
+++ backend/id-checkpoint/src/test/java/com/Fernandoezz/userservice/id_checkpoint/IdCheckpointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Fernandoezz.userservice.id_checkpoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdCheckpointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
